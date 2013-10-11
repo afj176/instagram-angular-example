c0a1cfcd205dff55a7642fa596909bc4185f5a03
Antonio Fernandes <afj176@gmail.com>
2013-10-11 09:24:26 -0400

    fixed formatting

--- js/instagram/controller.js
+++ js/instagram/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('InstagramCtrl', ['$scope', 'Instagram', 
+app.controller('InstagramCtrl', ['$scope', 'Instagram',
 	function($scope, Instagram) {
 
 		$scope.example1 = {
@@ -15,29 +15,29 @@ app.controller('InstagramCtrl', ['$scope', 'Instagram',
 			hash: 'examples'
 		};
 
-		var instagramSuccess = function(scope, res){
-	    	if(res.meta.code !== 200){
-	    		scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
-	    		return;
-	    	}		    
-	    	if(res.data.length > 0){
-	    		scope.items = res.data;
-	    	}else{
-	    		scope.error = "This hashtag has returned no results";	
-	    	}
+		var instagramSuccess = function(scope, res) {
+			if (res.meta.code !== 200) {
+				scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
+				return;
+			}
+			if (res.data.length > 0) {
+				scope.items = res.data;
+			} else {
+				scope.error = "This hashtag has returned no results";
+			}
 		};
 
-	    Instagram.get(9, $scope.example1.hash).success(function(response) {
+		Instagram.get(9, $scope.example1.hash).success(function(response) {
 			instagramSuccess($scope.example1, response);
-	    });
+		});
 
-	    Instagram.get(9, $scope.example2.hash).success(function(response) {
-	    	instagramSuccess($scope.example2, response);
-	    });
+		Instagram.get(9, $scope.example2.hash).success(function(response) {
+			instagramSuccess($scope.example2, response);
+		});
 
-	    Instagram.get(9, $scope.example3.hash).success(function(response) {
-	    	instagramSuccess($scope.example3, response);		    	
-	    });
+		Instagram.get(9, $scope.example3.hash).success(function(response) {
+			instagramSuccess($scope.example3, response);
+		});
 
-  	}
+	}
 ]);
